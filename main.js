92e96770434f4644571f96ef9a83b76f74564aef
Débora Arakaki <debo.arakaki@gmail.com>
2024-10-13 17:02:35 -0400

    main.js

--- main.js
+++ main.js
@@ -1,50 +1,16 @@
-const form = document.getElementById('form-deposito');
-const nomeBeneficiario = document.getElementById('nome-beneficiario');
-let formEValido = false;
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-function validaNome(nomeCompleto) {
-    const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length >= 2;
-}
+    const campoA = parseFloat(document.getElementById('campoA').value);
+    const campoB = parseFloat(document.getElementById('campoB').value);
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+    const messageDiv = document.getElementById('message');
 
-    const numeroContaBeneficiario = document.getElementById('numero-conta');
-    const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente: <b>${nomeBeneficiario.value}</b>- conta: <b>${numeroContaBeneficiario.value}</b>`;
-
-    formEValido = validaNome(nomeBeneficiario.value)
-    if (formEValido) {
-        const containerMensagemSucesso = document.querySelector('.success-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
-
-        nomeBeneficiario.value = '';
-        numeroContaBeneficiario.value = '';
-        valorDeposito.value = '';
+    if (campoB > campoA) {
+        messageDiv.textContent = 'Formulário válido! Campo B é maior que Campo A.';
+        messageDiv.className = 'message success';
     } else {
-        nomeBeneficiario.style.border = '1px solid red';
-        document.querySelector('.error-message').style.display = 'block';
-    }
-})
-
-nomeBeneficiario.addEventListener('keyup', function(e){
-    console.log(e.target.value);
-    formEValido = validaNome(e.target.value);
-
-    if (formEValido) {
-        nomeBeneficiario.classList.add('error');
-        document.querySelector('.error-message').style.display = 'none';
-    } else {
-        nomeBeneficiario.classList.remove('error');
-        document.querySelector('.error-message').style.display = 'block';
-    }
-
-    // Adiciona a classe 'error' apenas se houver um único nome
-    if (e.target.value.trim().split(' ').length === 1) {
-        nomeBeneficiario.classList.add('error');
-    } else {
-        nomeBeneficiario.classList.remove('error');
+        messageDiv.textContent = 'Formulário inválido! Campo B deve ser maior que Campo A.';
+        messageDiv.className = 'message error';
     }
 });
